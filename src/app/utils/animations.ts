d6ca283ad6eb077c6b13d9decc13209b216327e1
hungify <nmhungify@gmail.com>
2024-04-02 22:15:41 +0700

    Add animations utility and index file

---
+++ src/app/utils/animations.ts
@@ -0,0 +1,53 @@
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (
+  target: string,
+  animationProps: gsap.TweenVars,
+  scrollProps?: ScrollTrigger.Vars
+) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
+export const animateWithGsapTimeline = (
+  timeline: gsap.core.Timeline,
+  rotationRef: React.MutableRefObject<any>,
+  rotationState: number,
+  firstTarget: string,
+  secondTarget: string,
+  animationProps: gsap.TweenVars
+) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
+};
---
+++ src/app/utils/index.ts
@@ -0,0 +1,31 @@
+export const heroImg = "/assets/images/hero.jpeg" as const;
+
+export const heroVideo = "/assets/videos/hero.mp4" as const;
+export const smallHeroVideo = "/assets/videos/smallHero.mp4" as const;
+export const highlightFirstVideo =
+  "/assets/videos/highlight-first.mp4" as const;
+export const highlightSecondVideo =
+  "/assets/videos/hightlight-third.mp4" as const;
+export const highlightThirdVideo = "/assets/videos/hightlight-sec.mp4" as const;
+export const highlightFourthVideo =
+  "/assets/videos/hightlight-fourth.mp4" as const;
+export const exploreVideo = "/assets/videos/explore.mp4" as const;
+export const frameVideo = "/assets/videos/frame.mp4" as const;
+
+export const appleImg = "/assets/images/apple.svg" as const;
+export const searchImg = "/assets/images/search.svg" as const;
+export const bagImg = "/assets/images/bag.svg" as const;
+export const watchImg = "/assets/images/watch.svg" as const;
+export const rightImg = "/assets/images/right.svg" as const;
+export const replayImg = "/assets/images/replay.svg" as const;
+export const playImg = "/assets/images/play.svg" as const;
+export const pauseImg = "/assets/images/pause.svg" as const;
+
+export const yellowImg = "/assets/images/yellow.jpg" as const;
+export const blueImg = "/assets/images/blue.jpg" as const;
+export const whiteImg = "/assets/images/white.jpg" as const;
+export const blackImg = "/assets/images/black.jpg" as const;
+export const explore1Img = "/assets/images/explore1.jpg" as const;
+export const explore2Img = "/assets/images/explore2.jpg" as const;
+export const chipImg = "/assets/images/chip.jpeg" as const;
+export const frameImg = "/assets/images/frame.png" as const;
